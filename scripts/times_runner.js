2a728836ab073f266784269e547185d893bd96d7
Per Eckerdal <peck@spotify.com>
2014-08-05 21:24:05 +0200

    Move data into server folder
    We're going to need it there

--- scripts/times_runner.js
+++ scripts/times_runner.js
@@ -3,8 +3,8 @@ var times = require("../server/source/times");
 var distance = require("../server/source/distance");
 var weights = require("../server/source/weights");
 
-var concerts = JSON.parse(String(fs.readFileSync("data/concerts.json")));
-var venues = JSON.parse(String(fs.readFileSync("data/venues.json")));
+var concerts = JSON.parse(String(fs.readFileSync("server/data/concerts.json")));
+var venues = JSON.parse(String(fs.readFileSync("server/data/venues.json")));
 
 var schedule = times.findOptimalSchedule(distance.makeDistanceFunction(venues, "walking", 2), concerts, weights.weightsForPopularity(concerts, 5, 1));
 console.log(schedule.map(function(artist) {
