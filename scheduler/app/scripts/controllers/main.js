931295a20da259668e0aae3044edce7cfbeb3313
Per Eckerdal <peck@spotify.com>
2014-08-05 18:39:03 +0200

    First draft of activity list

--- scheduler/app/scripts/controllers/main.js
+++ scheduler/app/scripts/controllers/main.js
@@ -12,4 +12,33 @@ angular.module('schedulerApp')
     $scope.transportationMethod = 'walking';
     $scope.timeOptimist = 500;
     $scope.artistSize = 500;
+
+    $scope.days = [
+      {
+        name: 'torsdag',
+        activities: [
+          {
+            type: 'concert',
+            artistName: 'Yung Lean & Sad Boys',
+            venueName: 'Dungen',
+            startingTime: '18:00',
+            duration: '30m'
+          },
+          {
+            type: 'transit',
+            methodVerb: 'Walk',
+            fromVenueName: 'Dungen',
+            toVenueName: 'Azalea',
+            duration: '18m'
+          },
+          {
+            type: 'concert',
+            artistName: 'The National',
+            venueName: 'Azalea',
+            startingTime: '20:00',
+            duration: '1h 15m'
+          }
+        ]
+      }
+    ];
   });
