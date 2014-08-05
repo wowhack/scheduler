fbb9f1bad148923ca51f845872b1c9d6024f4354
Per Eckerdal <peck@spotify.com>
2014-08-05 22:45:10 +0200

    Make activity endpoint actually do something

--- server/routes/activities.js
+++ server/routes/activities.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var request = require('request');
+var fs = require('fs');
+var times = require("../source/times");
+var distance = require("../source/distance");
+var weights = require("../source/weights");
+var activities = require("../source/activities");
+
+var concerts = JSON.parse(String(fs.readFileSync(__dirname + "/../data/concerts.json")));
+var venues = JSON.parse(String(fs.readFileSync(__dirname + "/../data/venues.json")));
 
 function get_activities(req, res) {
-  res.send({ 'hello': 'world' });
+  var mode = req.query.mode || "walking";
+  var popularityWeight = parseFloat(req.query.popularity || "0.5");
+  var distanceMultiplier = parseFloat(req.query.distance || "1");
+
+  var schedule = times.findOptimalSchedule(distance.makeDistanceFunction(venues, mode, distanceMultiplier), concerts, weights.weightsForPopularity(concerts, 5, popularityWeight));
+  res.send(activities.scheduleToActivities(concerts, venues, mode, schedule));
 }
 
 module.exports = function() {
