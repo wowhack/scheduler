29749f9e8ebef940237fd7a8ea5a87766b4a3c56
Per Eckerdal <peck@spotify.com>
2014-08-05 17:02:04 +0200

    Add yeoman based angular template app

---
+++ scheduler/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name schedulerApp
+ * @description
+ * # schedulerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('schedulerApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ scheduler/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name schedulerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the schedulerApp
+ */
+angular.module('schedulerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
