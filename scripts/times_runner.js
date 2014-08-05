1ebf0d55df669abd1638bec2fd646ac904f87612
Per Eckerdal <peck@spotify.com>
2014-08-05 22:29:56 +0200

    First draft of activities generator

--- scripts/times_runner.js
+++ scripts/times_runner.js
@@ -3,14 +3,100 @@ var times = require("../server/source/times");
 var distance = require("../server/source/distance");
 var weights = require("../server/source/weights");
 
+function adjustedEventDay(startTime) {
+  return Math.floor(startTime / 60 / 24 - 0.25) + 1;
+}
+
+function adjustedEventTime(startTime) {
+  var timeOfDay = startTime % (60 * 24);
+  if (timeOfDay < 6*60) timeOfDay += 1440;
+  return timeOfDay;
+}
+
+var dayNames = ["Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var methodVerbs = {
+  "walking": "Walk",
+  "driving": "Drive",
+  "bicycling": "Cycle"
+};
+
+function scheduleToActivities(concerts, venues, mode, schedule) {
+  var artists = {};
+  concerts.forEach(function(concert) {
+    artists[concert['artist-id']] = concert;
+  });
+
+  function eventStartTime(artistId) {
+    return artists[artistId]['start-time'];
+  }
+
+  // Activities is an array of UI-readable concert descriptors
+  var activities = schedule.map(function(artistId) {
+    var artist = artists[artistId];
+
+    return {
+      type: 'concert',
+      artistName: artist['artist-name'],
+      artistId: artist['artist-id'],
+      venueName: venues[artist['venue']].name,
+      venueId: artist['venue'],
+      startingTime: adjustedEventTime(artist['start-time']),
+      duration: artist['end-time'] - artist['start-time']
+    };
+  });
+
+
+  // daysWithoutTransits is an array of UI-readable day descriptors (which contain concert descriptors)
+  var daysWithoutTransits = activities.reduce(function(previousValue, activity) {
+    var lastEventDay = previousValue.length == 0 ? -100 : adjustedEventDay(eventStartTime(previousValue[previousValue.length - 1].activities[0].artistId));
+    var currentEventDay = adjustedEventDay(eventStartTime(activity.artistId));
+    if (lastEventDay != currentEventDay) {
+      return previousValue.concat({
+        name: dayNames[currentEventDay],
+        day: currentEventDay,
+        activities: [activity]
+      });
+    } else {
+      previousValue[previousValue.length - 1].activities.push(activity);
+      return previousValue;
+    }
+  }, []);
+
+
+  // days is like daysWithoutTransits but with transit activities added
+  var days = daysWithoutTransits.map(function(day) {
+    day.activities = day.activities.reduce(function(previousValue, activity) {
+      var lastVenueId = previousValue.length == 0 ? null : previousValue[previousValue.length - 1].venueId;
+      var currentVenueId = activity.venueId;
+
+      var thingsToAdd = [];
+
+      if (lastVenueId !== null && lastVenueId != currentVenueId) {
+        thingsToAdd.push({
+          type: 'transit',
+          methodVerb: methodVerbs[mode],
+          fromVenueId: lastVenueId,
+          fromVenueName: venues[lastVenueId].name,
+          toVenueId: currentVenueId,
+          toVenueName: venues[currentVenueId].name,
+          duration: venues[lastVenueId].distances[mode][currentVenueId].duration,
+          distance: venues[lastVenueId].distances[mode][currentVenueId].distance
+        });
+      }
+
+      thingsToAdd.push(activity);
+
+      return previousValue.concat(thingsToAdd);
+    }, []);
+    return day;
+  });
+
+  return days;
+};
+
 var concerts = JSON.parse(String(fs.readFileSync("server/data/concerts.json")));
 var venues = JSON.parse(String(fs.readFileSync("server/data/venues.json")));
 
-var schedule = times.findOptimalSchedule(distance.makeDistanceFunction(venues, "walking", 2), concerts, weights.weightsForPopularity(concerts, 5, 1));
-console.log(schedule.map(function(artist) {
-  var artists = {};
-  concerts.forEach(function(concert) {
-    artists[concert['artist-id']] = concert['artist-popularity'];
-  })
-  return { artist: artist, pop: artists[artist] };
-}));
+var mode = "walking";
+var schedule = times.findOptimalSchedule(distance.makeDistanceFunction(venues, mode, 1), concerts, weights.weightsForPopularity(concerts, 5, 1));
+console.log(JSON.stringify(scheduleToActivities(concerts, venues, mode, schedule), null, 2));
