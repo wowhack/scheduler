12fc21037f98cc5125f26d57d1b508d7ce4090c7
Per Eckerdal <peck@spotify.com>
2014-08-06 11:12:16 +0200

    Add algorithm for calculating recommendation weights

---
+++ server/source/spotifyweights.js
@@ -0,0 +1,30 @@
+var fs = require("fs");
+
+var relation = JSON.parse(String(fs.readFileSync("server/data/relation.json")));
+
+var minDistance = 0.3;
+
+function extractBase62Id(id) {
+  return id.replace(/^spotify:artist:/, '');
+}
+
+function calculateWeights(concerts, myArtists) {
+  var weights = {};
+  concerts.forEach(function(concert) {
+    var weight = 0;
+    concert['spotify-uris'].forEach(function(concertArtistId) {
+      Object.keys(myArtists).forEach(function(myArtistId) {
+        var distance = relation[extractBase62Id(concertArtistId)][extractBase62Id(myArtistId)];
+        if (typeof distance === 'number') {
+          weight += 1/Math.max(minDistance, distance);
+        }
+      });
+    });
+
+    weights[concert['artist-id']] = 1 + weight * 5;
+  });
+
+  return weights;
+}
+
+exports.calculateWeights = calculateWeights;
