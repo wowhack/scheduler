c9212bc96b0b2d80871087f8125bcd4372911720
Per Eckerdal <peck@spotify.com>
2014-08-06 10:47:46 +0200

    Move artist retrieval into its own file

--- server/routes/artists.js
+++ server/routes/artists.js
@@ -1,38 +1,9 @@
 var express = require('express');
 var request = require('request');
+var artists = require('../source/artists');
 
 function get_artists(req, res, next) {
-  var artists = {};
-  var headers = { 'Authorization': 'Bearer ' + req.query.token };
-  var token16 = req.query.token.substr(0, 16);
-
-  function fetch(error, response, body) {
-    if (error || response.statusCode !== 200) {
-      next(error)
-      return;
-    }
-
-    body.items.forEach(function(item) {
-      (item.track.artists || []).forEach(function(artist) {
-        if (artists[artist.uri]) {
-          artists[artist.uri].count++;
-        } else {
-          artists[artist.uri] = { name: artist.name, count: 1 };
-        }
-      });
-    });
-
-    if (body.next) {
-      request.get({ url: body.next, headers: headers, json: true }, fetch);
-      return;
-    }
-
-    var count = Object.keys(artists).length;
-    res.send(artists);
-  };
-
-  var url = 'https://api.spotify.com/v1/me/tracks?limit=50';
-  request.get({ url: url, headers: headers, json: true }, fetch);
+  artists.getArtists(req.query.token, res.send.bind(res));
 };
 
 module.exports = function() {
---
+++ server/source/artists.js
@@ -0,0 +1,37 @@
+var request = require('request');
+
+function getArtists(token, cb) {
+  var artists = {};
+  var headers = { 'Authorization': 'Bearer ' + token };
+  var token16 = token.substr(0, 16);
+
+  function fetch(error, response, body) {
+    if (error || response.statusCode !== 200) {
+      next(error)
+      return;
+    }
+
+    body.items.forEach(function(item) {
+      (item.track.artists || []).forEach(function(artist) {
+        if (artists[artist.uri]) {
+          artists[artist.uri].count++;
+        } else {
+          artists[artist.uri] = { name: artist.name, count: 1 };
+        }
+      });
+    });
+
+    if (body.next) {
+      request.get({ url: body.next, headers: headers, json: true }, fetch);
+      return;
+    }
+
+    var count = Object.keys(artists).length;
+    cb(artists);
+  };
+
+  var url = 'https://api.spotify.com/v1/me/tracks?limit=50';
+  request.get({ url: url, headers: headers, json: true }, fetch);
+}
+
+exports.getArtists = getArtists;
