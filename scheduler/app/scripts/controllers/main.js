b8d0fdaa5092ae36cb8a43ddd993297275375dec
Per Eckerdal <peck@spotify.com>
2014-08-06 08:48:52 +0200

    Fix linter errors

--- scheduler/app/scripts/controllers/main.js
+++ scheduler/app/scripts/controllers/main.js
@@ -18,54 +18,54 @@ angular.module('schedulerApp')
 
     $scope.preferredConcerts = [
       {
-        "venue": "azalea",
-        "artist-id": "the-horrors",
-        "event-day": 0,
-        "artist-name": "The Horrors",
-        "spotify-uris": [
-          "spotify:artist:7EFB09NxZrMi9pGlOnuBpd"
+        'venue': 'azalea',
+        'artist-id': 'the-horrors',
+        'event-day': 0,
+        'artist-name': 'The Horrors',
+        'spotify-uris': [
+          'spotify:artist:7EFB09NxZrMi9pGlOnuBpd'
         ],
-        "start-time": 930,
-        "end-time": 990,
-        "artist-popularity": 44
+        'start-time': 930,
+        'end-time': 990,
+        'artist-popularity': 44
       },
       {
-        "venue": "azalea",
-        "artist-id": "tinariwen",
-        "event-day": 0,
-        "artist-name": "Tinariwen",
-        "spotify-uris": [
-          "spotify:artist:2sf2owtFSCvz2MLfxmNdkb"
+        'venue': 'azalea',
+        'artist-id': 'tinariwen',
+        'event-day': 0,
+        'artist-name': 'Tinariwen',
+        'spotify-uris': [
+          'spotify:artist:2sf2owtFSCvz2MLfxmNdkb'
         ],
-        "start-time": 1050,
-        "end-time": 1120,
-        "artist-popularity": 42
+        'start-time': 1050,
+        'end-time': 1120,
+        'artist-popularity': 42
       }
     ];
     $scope.removePreferredConcert = function(artistId) {
       $scope.preferredConcerts = $scope.preferredConcerts.filter(function(concert) {
-        return concert['artist-id'] != artistId;
+        return concert['artist-id'] !== artistId;
       });
-    }
+    };
 
     $scope.loginToSpotify = function() {
       var redirect = encodeURIComponent($window.location.href) + '{?}';
       $window.location.href = 'http://localhost:8888/login?redirect=' + redirect;
     };
 
-    $scope.fetchArtists = function(access_token) {
-      var artists_url = 'http://localhost:8888/artists?token=' + access_token;
-      $http({ method: 'GET', url: artists_url })
-          .success(function(data, status, headers, config) {
+    $scope.fetchArtists = function(accessToken) {
+      var artistsUrl = 'http://localhost:8888/artists?token=' + accessToken;
+      $http({ method: 'GET', url: artistsUrl })
+          .success(function(data) {
             $scope.artists = data;
           })
           .error(function(data, status, headers, config) {
             console.log('ERROR', data, status, headers, config);
           });
-    }
+    };
 
-    var access_token = $routeParams.access_token;
-    if (access_token) {
-      $scope.fetchArtists(access_token);
+    var accessToken = $routeParams.accessToken;
+    if (accessToken) {
+      $scope.fetchArtists(accessToken);
     }
   });
